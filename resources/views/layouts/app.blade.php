9c3ad17a9de10b2a0271b2771ad984c296b9904b
Caleb Nkunze <nkunzecaleb@gmail.com>
2024-06-08 23:19:05 +0300

    Add projects and create projects to menu bar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,20 @@
 <body>
     <div id="app">
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" href="#">Task Manager</a>
+            <a class="navbar-brand" href="{{ route('home') }}">Task Manager</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('projects.index') }}">Projects</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('projects.create') }}">Create Project</a>
+                    </li>
+                </ul>
+            </div>
         </nav>
 
         <main class="py-4">
