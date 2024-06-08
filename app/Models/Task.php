0ded4f6e4ea857ffdf38be4da4c9c5e929f92c49
Caleb Nkunze <nkunzecaleb@gmail.com>
2024-06-08 23:10:25 +0300

    Add name and priority as fillables in the tasks model

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -2,10 +2,16 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    use HasFactory;
+    protected $fillable = ['name', 'priority'];
+
+    // Define any relationships here
+    public function project()
+    {
+        return $this->belongsTo(Project::class);
+    }
 }
+
