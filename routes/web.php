1abd7224e1bfb6e20f0de515fd389edabbd4819e
Caleb Nkunze <nkunzecaleb@gmail.com>
2024-06-08 22:00:47 +0300

    Add route for reordering the tasks for a specific project

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,5 @@ use App\Http\Controllers\ProjectController;
 
 Route::resource('projects', ProjectController::class);
 Route::resource('projects.tasks', TaskController::class);
+
+Route::post('projects/{project}/tasks/reorder', [TaskController::class, 'reorder'])->name('projects.tasks.reorder');
