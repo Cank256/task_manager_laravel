a7f0b451c51aafde85f8733be65504e6d5c4c1b2
Caleb Nkunze <nkunzecaleb@gmail.com>
2024-06-08 23:41:44 +0300

    Beautify project tasks list view

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -3,15 +3,19 @@
 @section('content')
 <div class="container">
     <h1>{{ $project->name }}</h1>
-    <a href="{{ route('tasks.create', $project) }}" class="btn btn-primary">Add Task</a>
-    <ul id="tasks">
+    <a href="{{ route('tasks.create', $project) }}" class="btn btn-primary mb-3">Add Task</a>
+    <ul class="list-group tasks">
         @foreach ($tasks as $task)
-            <li data-id="{{ $task->id }}">{{ $task->name }}
-                <form action="{{ route('tasks.destroy', [$project, $task]) }}" method="POST" style="display:inline;">
-                    @csrf
-                    @method('DELETE')
-                    <button type="submit" class="btn btn-danger">Delete</button>
-                </form>
+            <li class="list-group-item">
+                <a>{{ $task->name }}</a>
+                <div class="float-right">
+                    <a href="{{ route('tasks.edit', [$project, $task]) }}" class="btn btn-sm btn-secondary">Edit</a>
+                    <form action="{{ route('tasks.destroy', [$project, $task]) }}" method="POST" style="display:inline;">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="btn btn-sm btn-danger">Delete</button>
+                    </form>
+                </div>
             </li>
         @endforeach
     </ul>
